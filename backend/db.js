eac02088646c39265d676d8338ee5dc6d4f6d719
Jay Vigilla <JayRVigilla@gmail.com>
2020-08-10 17:38:19 -0700

    WIP deploy to Heroku

--- backend/db.js
+++ backend/db.js
@@ -2,9 +2,20 @@
 
 const { Client } = require("pg");
 
-const client = new Client(process.env.DATABASE_URL || "postgresql:///microblog");
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+ssl: {
+  rejectUnauthorized: false
+}} || "postgresql:///microblog");
 
 client.connect();
 
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
 module.exports = client;
