ed0c189d0ac180436ec6d639b434cb55c126fdf9
Jay Vigilla <JayRVigilla@gmail.com>
2020-08-12 16:56:05 -0700

    updated Nav css to match main branch

--- src/Nav.js
+++ src/Nav.js
@@ -3,7 +3,8 @@ import './Nav.css';
 import { NavLink } from 'react-router-dom';
 
 
-/** Nav: Component that renders the navbar, with links to go back to the main blog page or to add a new post
+/** Nav: Component that renders the navbar
+ *    links to main blog page or to add new post
  *    - Used in App component
  */
 
@@ -11,9 +12,11 @@ function Nav() {
   return (
     <div className="Nav">
       <div className="Nav-contents">
-        <h1>Microblog</h1>
-        <h4>Get into the Rithm of blogging.</h4>
-        <div>
+        <div className="logo">
+          <h4>＿φ(°-°=)</h4>
+          <h4>ZhangGa</h4>
+        </div>
+        <div className="nav-links">
           <NavLink className="Nav-NavLink" to="">Blog</NavLink>
           <NavLink className="Nav-NavLink" to="/new">Add new post</NavLink>
         </div>
--- src/PostList.js
+++ src/PostList.js
@@ -11,20 +11,19 @@ import PostCard from './PostCard';
 function PostList({ idToPost }) {
 
   let postsList = Object.keys(idToPost).map(id => idToPost[id]);
- 
+
   const postCards = postsList.map(({ title, description, key }) => (
-    <PostCard 
-      key={key} 
+    <PostCard
+      key={key}
       id={key}
-      title={title} 
+      title={title}
       description={description}></PostCard>
   ))
 
 
+
   return (
     <div className="PostList">
-      <h3>＿φ(°-°=)</h3>
-      <p>Welcome to Microblog!</p>
       <div>
         {postCards}
       </div>
