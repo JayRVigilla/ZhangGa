517d53c2db04afd31e0dd39dcad2214d8db9e83e
Jay Vigilla <JayRVigilla@gmail.com>
2020-08-13 00:11:25 -0700

    comment component looks like main branch

--- src/Comment.js
+++ src/Comment.js
@@ -1,21 +1,23 @@
 import React from "react";
-import './Comment.css';
+import "./Comment.css";
+import { ListGroupItem } from "reactstrap";
 
 /** Comment: Component that renders each list item comment inside CommentsList component
  *    - Holds props of comment, deleteComment, postId, and commentId
  */
 
-function Comment({comment, deleteComment, postId, commentId}) {
-
-  function handleRemove(){
+function Comment({ comment, deleteComment, postId, commentId }) {
+  function handleRemove() {
     deleteComment(postId, commentId);
   }
 
   return (
+    <ListGroupItem>
     <div className="Comment">
-      <li className="Comment-list-item">{comment}</li>
-      <button className="Comment-button btn btn-danger py-0 px-2" onClick={handleRemove}><b>x</b></button>
-    </div>
+      {comment}
+      <button className="Comment-button btn btn-danger py-0 px-2"onClick={handleRemove}><b>x</b></button>
+      </div>
+      </ListGroupItem>
   );
 }
 
